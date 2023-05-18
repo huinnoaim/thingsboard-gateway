a35f4482cfd0ff91e8c068a19cc6b4a0b5c8fc17
gesazumoo <gesazumoo@huinnoaim.com>
2023-05-18 19:21:20 +0900

    feat:hr

---
+++ converters/hr.py
@@ -0,0 +1,28 @@
+from redis_client import Redis
+from mqtt_client import MQTTClient
+
+redis = Redis.from_cfgfile()
+
+ptrn = f'ecg:*'
+res = redis.keys(ptrn)
+print(res)
+
+
+mqtt_client = MQTTClient(
+    url="a3480422f7d584c65aaf2b9fa258fead-500139692.ap-northeast-2.elb.amazonaws.com",
+    port=8081,
+    token="pM8bzmflJvCCuvcSKwhB"
+)
+
+# MQTT 브로커에 연결
+mqtt_client.connect()
+
+payload = {}
+payload['0023A0000011'] = [{}]
+mqtt_client.pub(
+    topic="v1/gateway/telemetry",
+    message= '{"0023A0000011": [{"ts": 1483228800000,"values": {"temperature": 42,"humidity": 80}},{"ts": 1483228801000,"values": {"temperature": 43,"humidity": 82}}],"0023P1000200": [{"ts": 1483228800000,"values": {"temperature": 42,"humidity": 80}}]}'
+)
+
+# 연결 종료
+mqtt_client.disconnect()
--- converters/mqtt_client.py
+++ converters/mqtt_client.py
@@ -1,105 +1,28 @@
-from __future__ import annotations
-import time
-import yaml
-import threading
-from os import path
-from pathlib import Path
-from typing import Union
-import logging
-import dataclasses as dc
+import paho.mqtt.client as mqtt
 
-from tb_device_mqtt import TBDeviceMqttClient
+class MQTTClient:
+    def __init__(self, url, port, token):
+        self.url = url
+        self.port = port
+        self.token = token
+        self.client = mqtt.Client()
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__file__)
+    def on_connect(self, client, userdata, flags, rc):
+        print(f"Connected with result code {rc}")
 
+    def on_publish(self, client, userdata, mid):
+        print("Message published")
 
-@dc.dataclass
-class DeviceData:
-    device: Union[str, None]
-    attribute: Union[str, None]
-    telemetry: Union[str, None]
+    def connect(self):
+        self.client.username_pw_set(self.token, "")
+        self.client.on_connect = self.on_connect
+        self.client.on_publish = self.on_publish
+        self.client.connect(self.url, self.port)
+        self.client.loop_start()
 
+    def pub(self, topic, message, qos=1):
+        self.client.publish(topic, message, qos)
 
-class SingletonType(type):
-    '''It restricts a class as Singleton.
-
-    Refer the belows:
-    1. https://blog.ionelmc.ro/2015/02/09/understanding-python-metaclasses/
-    2. https://dojang.io/mod/page/view.php?id=2468
-    '''
-    def __call__(cls, *args, **kwargs):
-        try:
-            return cls.__instance
-        except AttributeError:
-            cls.__instance = super().__call__(*args, **kwargs)
-            return cls.__instance
-
-
-class TBMqttClient(threading.Thread, metaclass=SingletonType):
-    '''Thingsboard MQTT Client.
-
-    Refer the belows:
-    1. https://thingsboard.io/docs/reference/python-client-sdk/
-    2. https://thingsboard.io/docs/reference/mqtt-api/
-    '''
-    def __init__(self, host: str, port: int, security_token: str) -> None:
-        super().__init__()
-        self.setName = 'Thingsboard MQTT client'
-        self.setDaemon = True
-
-        self.__stopped = False
-        self.client = TBDeviceMqttClient(host=host, port=port, username=security_token)
-        self.start()
-
-    def send_bulk(self, bulkdata: list[DeviceData]):
-        '''It sends bulk data to Thingsboard Server.
-        '''
-        for data in bulkdata:
-            self.client.send_telemetry(data.telemetry)
-            self.client.send_attributes(data.attribute)
-
-    def run(self):
-        try:
-            while not self.client.is_connected() and not self.__stopped:
-                if self.__stopped:
-                    break
-                logger.info("connecting to ThingsBoard")
-                try:
-                    self.client.connect()
-                    logger.debug('connected')
-                except ConnectionRefusedError as e:
-                    logger.error('ConnectionRefusedError', e)
-                    pass
-                except Exception as e:
-                    logger.exception(e)
-                time.sleep(2)
-        except Exception as e:
-            logger.exception(e)
-            time.sleep(10)
-
-        while not self.__stopped:
-            try:
-                if not self.__stopped:
-                    time.sleep(.2)
-                else:
-                    break
-            except KeyboardInterrupt:
-                self.__stopped = True
-            except Exception as e:
-                logger.exception(e)
-
-    @staticmethod
-    def from_cfgfile(fpath: Union[Path, None] = None) -> TBMqttClient:
-        dirname = path.dirname(path.abspath(__file__))
-        cfg_file = dirname + '/config/client.yaml'.replace('/', path.sep)
-        cfg_file = cfg_file if fpath is None else fpath
-
-        with open(cfg_file) as general_config:
-            cfg = yaml.safe_load(general_config)
-
-        tb_cfg = cfg['thingsboard']
-        host = tb_cfg['host']
-        port = tb_cfg['port']
-        security_token = tb_cfg['security']['accessToken']
-        return TBMqttClient(host, port, security_token)
+    def disconnect(self):
+        self.client.loop_stop()
+        self.client.disconnect()
