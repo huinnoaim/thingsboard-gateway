f4ac4dea36c2ad9d4b9bf347c20576c1d3fc4e2c
Seungbae Ji <seungbae.ji@gmail.com>
2023-08-24 18:17:50 +0900

    #173 fmt: Applying the black formatting

--- modules/alarm/main.py
+++ modules/alarm/main.py
@@ -18,15 +18,16 @@ from alarm.alarm_manager import AlarmManager
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.DEBUG)
 
+
 class Envs(NamedTuple):
     MQTT_MOSQUITTO_URL: str
     MQTT_MOSQUITTO_PORT: int
 
     @staticmethod
-    def getenv(fpath: Path| None = None) -> Envs:
+    def getenv(fpath: Path | None = None) -> Envs:
         load_dotenv(fpath)
         kwargs = {}
-        for field in  Envs.__dict__['_fields']:
+        for field in Envs.__dict__["_fields"]:
             casting = getattr(builtins, Envs.get_typing(field), None)
             value = os.getenv(field)
             kwargs[field] = casting(value) if casting else value
@@ -38,21 +39,28 @@ class Envs(NamedTuple):
 
 
 def update_cfgfile(envs: Envs, cfg_fpath: Path):
-    '''It updates the config yaml file by using envs.
-    '''
-    with open(cfg_fpath, 'r') as f:
+    """It updates the config yaml file by using envs."""
+    with open(cfg_fpath, "r") as f:
         yaml_data = yaml.load(f, Loader=yaml.SafeLoader)
-        yaml_data['mqtt']['mosquitto']['url'] = envs.MQTT_MOSQUITTO_URL
-        yaml_data['mqtt']['mosquitto']['port'] = envs.MQTT_MOSQUITTO_PORT
+        yaml_data["mqtt"]["mosquitto"]["url"] = envs.MQTT_MOSQUITTO_URL
+        yaml_data["mqtt"]["mosquitto"]["port"] = envs.MQTT_MOSQUITTO_PORT
 
-    with open(cfg_fpath, 'w') as f:
+    with open(cfg_fpath, "w") as f:
         yaml.safe_dump(yaml_data, f)
 
 
 def get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='ECG Converters')
-    parser.add_argument('--env-fpath', '-e', default='.env', help='envfile file path', type=Path)
-    parser.add_argument('--cfg-fpath', '-c', default='./config.yaml', help='config file path', type=os.path.abspath)
+    parser = argparse.ArgumentParser(description="ECG Converters")
+    parser.add_argument(
+        "--env-fpath", "-e", default=".env", help="envfile file path", type=Path
+    )
+    parser.add_argument(
+        "--cfg-fpath",
+        "-c",
+        default="./config.yaml",
+        help="config file path",
+        type=os.path.abspath,
+    )
     return parser.parse_args()
 
 
@@ -63,7 +71,7 @@ def main(args: argparse.Namespace):
     logger.info(envs)
     logger.info(args.cfg_fpath)
 
-    client = MQTTClient.from_cfgfile('mosquitto', args.cfg_fpath)
+    client = MQTTClient.from_cfgfile("mosquitto", args.cfg_fpath)
     __alarm_manager = AlarmManager(client)
 
     def handle_hr_message(client, userdata, msg):
@@ -74,7 +82,7 @@ def main(args: argparse.Namespace):
         sensor_type = payload_slice[0][5:]
         value = params_slice[1]
 
-        __alarm_manager.check_alarm( params_slice[0].split("=")[1], sensor_type, value)
+        __alarm_manager.check_alarm(params_slice[0].split("=")[1], sensor_type, value)
 
     def handle_alarm_message(client, userdata, msg):
         payload = json.loads(msg.payload.decode())
@@ -82,9 +90,9 @@ def main(args: argparse.Namespace):
 
     def handle_alarm_rule_message(client, userdata, msg):
         payload = json.loads(msg.payload.decode())
-        if 'from' in payload and payload['from'] == 'pmc' :
+        if "from" in payload and payload["from"] == "pmc":
             __alarm_manager.get_alarm_rule()
-        else :
+        else:
             __alarm_manager.upsert_alarm_rule(payload)
 
     def handle_reload_message(client, userdata, msg):
@@ -94,7 +102,7 @@ def main(args: argparse.Namespace):
         "devices/hr": handle_hr_message,
         "alarms/#": handle_alarm_message,
         "noti/alarm-rules/#": handle_alarm_rule_message,
-        "noti/reload": handle_reload_message
+        "noti/reload": handle_reload_message,
     }
 
     def on_message(client, userdata, msg):
@@ -116,6 +124,7 @@ def main(args: argparse.Namespace):
 
     except:
         client.disconnect()
+
 
 if __name__ == "__main__":
     args = get_args()
